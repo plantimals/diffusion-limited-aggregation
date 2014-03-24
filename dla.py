8260c116e86a25c9fd95a7875ba8d18a8bafa3f1
arrogantrobot <arrogantrobot@gmail.com>
2014-03-23 22:39:23 -0500

    change point dla to use correct is_near function

--- dla.py
+++ dla.py
@@ -4,6 +4,7 @@ import png
 import randomwalk
 import itertools
 import random
+import sys
 
 p_x = 300
 p_y = 300
@@ -17,35 +18,41 @@ def is_near(point, platten):
   up = platten[x][y + 1] == on_square
   left = platten[x - 1][y] == on_square
   down = platten[x][y - 1] == on_square
-  return right or up or left or down
+  q1 = platten[x + 1][y + 1] == on_square
+  q2 = platten[x - 1][y + 1] == on_square
+  q3 = platten[x - 1][y - 1] == on_square
+  q4 = platten[x + 1][y - 1] == on_square
+  return right or up or left or down or q1 or q2 or q3 or q4
 
 def get_start():
-  x = random.randrange(-p_x,2*p_x)
-  y = random.randrange(-p_y,2*p_y)
+  x = random.randrange(0,p_x)
+  y = random.randrange(0,p_y)
   return (x,y)
 
 def get_grey(x):
   return 255 if x == 1 else 0
 
 def write_png(platten):
-  f = open('dla.png', 'wb')      # binary mode is important
+  f = open('dla_new.png', 'wb')      # binary mode is important
   w = png.Writer(p_x, p_y, greyscale=True)
   w.write(f, platten)
   f.close()
 
+#if len(sys.argv) > 1:
+#  print "loading.."
+#  f = png.Reader(file=open(sys.argv[1]))
+#  print f.read()
+
 platten = []
 for _ in range(p_y):
-  platten.append(list(itertools.repeat(0,p_x)))
-platten[int(p_x/2)][int(p_y/2)]=on_square
+  platten.append(list(itertools.repeat(0, p_x)))
+platten[int(p_x/2)][int(p_y/2)] = on_square
 
-for n in range(100000):
-  x,y = get_start()
-  rw = randomwalk.RandomWalk(x,y)
-  while rw.count < 1000:
+for n in range(10000):
+  rw = randomwalk.RandomWalk(p_x,p_y)
+  while not is_near((rw.x,rw.y), platten):
     rw.iterate()
-    if is_near((rw.x,rw.y), platten):
-      platten[rw.x][rw.y] = on_square
-      break
+  platten[rw.x][rw.y] = on_square
   #print "{0}, {1}".format(rw.x, rw.y)
 
 
